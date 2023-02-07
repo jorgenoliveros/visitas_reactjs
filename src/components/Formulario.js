9b24af9dddf35cdc54c0e91f7619f50e2062f687
Jorgen Oliveros <jorgen.oliveros@gmail.com>
2023-02-06 22:54:15 -0300

    Creacion de la carpeta componentes y archivo formulario.js

---
+++ src/components/Formulario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Formulario = () => {
+
+    return ( 
+        <h2><i className='bx bxs-user-plus' ></i>   Agendar Visita</h2>
+     );
+}
+
+export default Formulario;

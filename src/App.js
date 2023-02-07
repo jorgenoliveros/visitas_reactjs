52de6e9011aebb9d2d4f3b6cb900b66565e38ff1
Jorgen Oliveros <jorgen.oliveros@gmail.com>
2023-02-06 22:52:56 -0300

    Creacion de las secciones y prueba del formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Formulario from './components/Formulario ';
 
 function App() {
   return (
-    <h1>Registro de Visitantes</h1>
+    <Fragment>
+      <div className="titulo">
+        <div className="icono">
+          <i className='bx bx-buildings' ></i>
+        </div>
+        <h1>Registro de Visitantes</h1>
+      </div>
+
+      <div className="container">
+        <div className="Row">
+          <div className="one-half column contenedor">
+            <Formulario />
+          </div>
+          <div className="one-half column contenedor">
+            <h2><i className='bx bxs-user-detail' ></i>  Visitantes</h2>  
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
